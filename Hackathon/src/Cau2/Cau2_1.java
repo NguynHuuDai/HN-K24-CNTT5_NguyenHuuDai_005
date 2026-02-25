58ff036f7ece63024e1df7f472b6b6c195eb5b72
NguynHuuDai <daizzt10@gmail.com>
2026-02-25 11:25:23 +0700

    Add files via upload
    Refactor array reversal logic to rotate array elements.

--- Hackathon/src/Cau2/Cau2_1.java
+++ Hackathon/src/Cau2/Cau2_1.java
@@ -1,34 +1,25 @@
 package Cau2;
 
 public class Cau2_1 {
+
     public static void main(String[] args) {
 
-        Number[] arr = new Number[100];
-
+        int[] arr = {1, 2, 3, 4, 5, 6};
         int k = 2;
-        arr = new Number[]{1, 2, 3, 4, 5, 6 };
 
-        for (int i = 0; i <= k; i++) {
-            Number temp = 0;
-            temp = arr[i];
-            arr[i] = arr[arr.length - i - 1];
-            arr[arr.length - i - 1] = temp;
+        int n = arr.length;
 
-        }
-        for (int i = k; i < arr.length - k; i++) {
-            Number temp = 0;
-            int n = 0;
-            temp = arr[i];
-            arr[i] = arr[arr.length - n - 1];
-            arr[arr.length - n - 1] = temp;
-        }
-        for (int i = 0; i < arr.length; i ++){
-            System.out.println(arr[i]);
-        }
+        k = k % n;
+        for (int step = 0; step < k; step++) {
+            int last = arr[n - 1];
+            for (int i = n - 1; i > 0; i--) {
+                arr[i] = arr[i - 1];
+            }
 
+            arr[0] = last;
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
-
-
-
 }
-
